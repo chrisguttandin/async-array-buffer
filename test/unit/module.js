6a77c164277f820ef3e067dd7c494b8cba1b589e
Christoph Guttandin <chrisguttandin@media-codings.com>
2020-06-19 14:12:04 +0200

    style: run prettier

--- test/unit/module.js
+++ test/unit/module.js
@@ -1,31 +1,23 @@
 import { allocate, connect, deallocate, disconnect, isSupported } from '../../src/module';
 
 describe('module', () => {
-
     describe('allocate()', () => {
-
         it('should allocate an ArrayBuffer', () => {
-            return allocate(1024)
-                .then((arrayBuffer) => {
-                    expect(arrayBuffer.byteLength).to.equal(1024);
-                });
+            return allocate(1024).then((arrayBuffer) => {
+                expect(arrayBuffer.byteLength).to.equal(1024);
+            });
         });
-
     });
 
     describe('connect()', () => {
-
         it('should connect a port', () => {
-            return connect()
-                .then((port) => {
-                    expect(port).to.be.an.instanceOf(MessagePort);
-                });
+            return connect().then((port) => {
+                expect(port).to.be.an.instanceOf(MessagePort);
+            });
         });
-
     });
 
     describe('deallocate()', () => {
-
         let arrayBuffer;
 
         beforeEach(() => {
@@ -35,28 +27,21 @@ describe('module', () => {
         it('should deallocate an ArrayBuffer', () => {
             return deallocate(arrayBuffer);
         });
-
     });
 
     describe('disconnect()', () => {
-
         let port;
 
-        beforeEach(() => connect()
-            .then((prt) => port = prt));
+        beforeEach(() => connect().then((prt) => (port = prt)));
 
         it('should disconnect a port', () => {
             return disconnect(port);
         });
-
     });
 
     describe('isSupported()', () => {
-
         it('should check the browser support', () => {
             return isSupported();
         });
-
     });
-
 });
